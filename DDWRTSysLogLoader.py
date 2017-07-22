1a327eb949a246a6cbe33269e46db3163c3c11bc
Ram Varra <ramvarra@hotmail.com>
2017-07-22 16:47:48 -0700

    ok

--- DDWRTSysLogLoader.py
+++ DDWRTSysLogLoader.py
@@ -8,24 +8,28 @@ import datetime
 import GeoIP
 import geoip2.database
 import ipaddress
+import platform
+import logging
 
+import rv.misc
 
 #---------------------------------------------------------------------------------------------------------------
 class ServerHelper:
 
     CITY_DB = r'/DATA/MAXMIND.GEOIP2/GeoLite2-City_20170704/GeoLite2-City.mmdb'
     ASN_DB = r'/DATA/MAXMIND.GEOIP2/GeoLite2-ASN_20170704/GeoLite2-ASN.mmdb'
+    ES_HOST_CONFIG = dict(hosts=['http://192.168.1.141:9200'], timeout=240)
 
     #--------------------------------------------------------------------------------------
     def __init__(self):
-        print("Receiver: init")
-        self.pat = re.compile(r"<4>(?P<dt>\w+\s+\d+\s+\d+:\d+:\d+)\s+kernel:\s+(?P<action>[A-Z]+)\s+(?P<opts>.*)")
+        self.pat1 = re.compile(r"<4>(?P<dt>\w+\s+\d+\s+\d+:\d+:\d+)\s+kernel:\s+(?P<action>[A-Z]+)\s+(?P<opts>.*)")
+        self.pat2 = r'<(?P<code>\d+)>(?P<dt>\w+\s+\d+\s+\d+:\d+:\d+)\s+(?P<fac>[^:]+):\s+(?P<rest>.*)'
         self.local_tz = tzlocal.get_localzone()
         self.gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
         self.city_reader = geoip2.database.Reader(self.CITY_DB)
         self.asn_reader = geoip2.database.Reader(self.ASN_DB)
-        self.es = elasticsearch.Elasticsearch(hosts=['http://localhost:9200'], timeout=240)
-        print("Receiver: init done")
+        self.es = elasticsearch.Elasticsearch(**self.ES_HOST_CONFIG)
+        logging.info("Connecting to Elasticsearch: {}".format(self.ES_HOST_CONFIG['hosts']))
 
     # --------------------------------------------------------------------------------------
     def geo_ip(self, ip):
@@ -49,15 +53,38 @@ class ServerHelper:
             pass
         return r
 
-    def setup_template(self):
-        template = {
-            "template": "routerlog-*",
-            "settings": {
+    # -------------------------------------------------------------------------------------------------------------------
+    def create_templates(self):
+        rl_props = {
+            "SRC_LOC": { "type": "geo_point"},
+            "DST_LOC": { "type": "geo_point"},
+            "SRC": {"type": "ip"},
+            "DST": {"type": "ip"},
+            "TS": {"type": "date"},
+        }
+
+        self.setup_template('routerlog', rl_props)
+
+        syslog_props = {
+            "TS": {"type": "date"},
+        }
+        self.setup_template('router_syslog', syslog_props)
+
+    #-------------------------------------------------------------------------------------------------------------------
+    def setup_template(self, doc_type, props, settings=None):
+        template_name = doc_type + '_template'
+        template_pattern = doc_type + '-*'
+        if settings is None:
+            settings = {
                 "number_of_shards": 2,
                 "number_of_replicas": 0
-            },
+            }
+
+        template = {
+            "template": template_pattern,
+            "settings": settings,
             "mappings": {
-                "routerlog": {
+                doc_type: {
                     "_all": {
                         "enabled": False
                     },
@@ -71,50 +98,52 @@ class ServerHelper:
                             }
                         }
                     ],
-                    "properties": {
-                        "SRC_LOC": {
-                            "type": "geo_point"
-                        },
-                        "DST_LOC": {
-                            "type": "geo_point"
-                        },
-                        "SRC": {
-                            "type": "ip"
-                        },
-                        "DST": {
-                            "type": "ip"
-                        },
-                        "TS": {
-                            "type": "date"
-                        }
-                    }
+                    "properties": props
                 }
             }
         }
 
 
         try:
-            self.es.indices.delete_template('routerlog_template')
+            self.es.indices.delete_template(template_name)
         except:
             pass
-        print('Creating template')
-        self.es.indices.put_template('routerlog_template', template)
+        logging.info('Creating template - {}'.format(template_name))
 
-    def index(self, rec):
-        ix = 'routerlog-{:%Y.%W}'.format(rec['TS'])
-        self.es.index(ix, doc_type = 'routerlog', body=rec)
+        ret = self.es.indices.put_template(template_name, template)
+        logging.info("Ret: {}".format(ret))
+
+
+    # ------------------------------------------------------------------------------------------
+    def index(self, doc_type, rec):
+        ix = '{}-{:%Y.%W}'.format(doc_type, rec['TS'])
+        self.es.index(ix, doc_type, body=rec)
+
+    # ------------------------------------------------------------------------------------------
+    def get_ts(self, ts_string):
+        ts = datetime.datetime.strptime(ts_string, '%b %d %H:%M:%S')
+        ts = self.local_tz.localize(ts.replace(year=datetime.date.today().year))
+        return ts
+
+    # ------------------------------------------------------------------------------------------
+    def get_fac(self, fac):
+        fac_n = ''
+        if '[' in fac:
+            fac, fac_n = fac.split('[')
+            fac_n = fac_n.rstrip(']')
+        return (fac, fac_n)
 
 #------------------------------------------------------------------------------------------
 class SyslogReceiver(socketserver.BaseRequestHandler):
+
     # --------------------------------------------------------------------------------------
     def handle(self):
         global server_helper
 
-        data = self.request[0].strip()
-        m = re.match(server_helper.pat, data.decode())
+        data = self.request[0].strip().decode()
+        m = re.match(server_helper.pat1, data)
         if m:
-            ts = datetime.datetime.strptime(m.group('dt'), '%b %d %H:%M:%S')
-            ts = server_helper.local_tz.localize(ts.replace(year=datetime.date.today().year))
+            ts = server_helper.get_ts(m.group('dt'))
             action = m.group('action')
             d = {'TS': ts, 'ACTION': action}
             for opt in m.group('opts').split():
@@ -126,20 +155,40 @@ class SyslogReceiver(socketserver.BaseRequestHandler):
             d.update({"SRC_{}".format(k): v for k,v in src.items()})
             dst = server_helper.geo_ip(d['DST'])
             d.update({"DST_{}".format(k): v for k, v in dst.items()})
-            server_helper.index(d)
-        else:
-            print("*UNKNOWN*: {}".format(data))
+            #logging.info("Writing {}".format(d))
+            server_helper.index('routerlog', d)
+            return
+
+        m = re.match(server_helper.pat2, data)
+        if m:
+            d = {'TS': server_helper.get_ts(m.group('dt'))}
+            d['CODE'] = m.group('code')
+            fac, fac_n = server_helper.get_fac(m.group('fac'))
+            d['FAC'] = fac
+            if fac_n:
+                d['FAC_N'] = fac_n
+            d['MSG'] = m.group('rest')
+            #logging.info("Writing {}".format(d))
+            server_helper.index('router_syslog', d)
+            return
+
+
+
+        logging.warning("*UNKNOWN*: {}".format(data))
+
 
 
 #-------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    HOST, PORT = 'UBU01', 514
+    HOST, PORT = platform.node(), 514
+    rv.misc.set_logging(log_dir='/var/log/esetl')
+
     server_helper = ServerHelper()
     if len(sys.argv) == 2:
         assert sys.argv[1] == 'initialize_template'
-        server_helper.setup_template()
+        server_helper.create_templates()
         sys.exit(1)
 
     with socketserver.UDPServer((HOST, PORT), SyslogReceiver) as server:
-        print('Running on ', (HOST, PORT))
+        logging.info('Running on {}'.format((HOST, PORT)))
         server.serve_forever()
---
+++ IISLoader.py
@@ -0,0 +1,166 @@
+import os, re, sys
+import elasticsearch
+import datetime
+import pytz
+import tzlocal
+import httpagentparser
+import logging
+import itertools
+import rv.misc
+import rv.geoip
+import rv.ESUtil
+
+
+class IISLogReader:
+    COLFMT = 's-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken'
+    ES_CONFIG = {'hosts': 'http://UBU01.ramvarra.com:9200', 'timeout': 240}
+    INDEX_PREFIX = 'iislog'
+    IIS_LOG_DIR = r'C:\inetpub\logs\LogFiles\W3SVC1'
+
+    def __init__(self):
+        self.regex = r'u_ex(?P<dt>\d+)\.log'
+        self._cols = [re.sub(r'[-\(\)]', '_', c).lower().rstrip('_') for c in self.COLFMT.split()]
+        self.local_tz = tzlocal.get_localzone()
+        self.geoip = rv.geoip.GeoIP()
+        self.esu = rv.ESUtil.ESUtil(**self.ES_CONFIG)
+        self.total_count = 0
+
+    def get_ua(self, uas):
+        '''
+        generate flat list of UA attributes
+        :param uas: ua string from client
+        :return: flat dict of detected ua attributes.
+        '''
+        d = httpagentparser.detect(uas)
+        r = {}
+
+        for k, v in d.items():
+            if isinstance(v, dict):
+                for k1, v1 in v.items():
+                    if v1:
+                        r[k + '_' + k1] = v1
+            else:
+                r[k] = v
+        if not r:
+            r['browser_name'] = uas
+        return r
+
+    def _get_dt(self, f):
+        if f is None:
+            return None
+
+        m = re.match(self.regex, f)
+        if m:
+            dts = m.group('dt')
+            dt = datetime.date(int(dts[:2]) + 2000, int(dts[2:4]), int(dts[4:]))
+            return dt
+        else:
+            return None
+
+    def get_file_entry(self, last_file=None):
+        flist = []
+        for f in os.listdir(self.IIS_LOG_DIR):
+            dt = self._get_dt(f)
+            if dt:
+                d = {'date': dt, 'file': f}
+                flist.append(d)
+
+        # sort files
+        flist = sorted(flist, key=lambda x: x['date'])
+        last_file_dt = self._get_dt(last_file)
+        for entry in flist:
+            if last_file_dt and entry['date'] < last_file_dt:
+                continue
+            yield entry
+
+    def getrec(self, last_file=None, last_offset=-1):
+        self.total_count = 0
+        for entry in self.get_file_entry(last_file):
+            full_f = os.path.join(self.IIS_LOG_DIR, entry['file'])
+            logging.info('Processing file: {} offset: {}'.format(full_f, last_offset))
+
+            with open(full_f) as fd:
+                if last_offset > 0:
+                    fd.seek(last_offset)
+                    last_offset = -1
+                cols = None
+                while True:
+                    l = fd.readline()
+                    offset = fd.tell()
+                    if not l:
+                        break
+                    l = l.rstrip()
+                    if l[0] == '#':
+                        continue
+                    values = l.split()
+                    s = values[0] + ' ' + values[1]
+                    ts = pytz.utc.localize(datetime.datetime.strptime(s, '%Y-%m-%d %H:%M:%S')).astimezone(self.local_tz)
+                    data = dict(zip(self._cols, values[2:]))
+                    for ip_col in ['c_ip', 's_ip']:
+                        r = self.geoip.lookup(data[ip_col])
+                        if r:
+                            data.update({ip_col + '_' + k: v for k, v in r.items()})
+
+                    # parse ua
+                    uas = data.get('cs_user_agent')
+                    if uas:
+                        data.update({'ua_' + k: v for k,v in self.get_ua(uas).items()})
+
+                    data.update({'ts': ts,
+                                 'offset': offset,
+                                 'file': entry['file'],
+                                 '_id': entry['file'] + ':' + str(offset),
+                                 '_index': "{}-{:%Y.%m}".format(self.INDEX_PREFIX, ts),
+                                 '_type': self.INDEX_PREFIX,
+                                 })
+                    self.total_count += 1
+                    yield data
+
+    def setup_template(self):
+        #recs = [r for i, r in zip(range(128), self.getrec())]
+        recs = itertools.islice(self.getrec(), 128)
+        props = self.esu.get_props_from_recs(recs)
+        for ip_col in ['c_ip', 's_ip']:
+            props[ip_col] = {'type': 'ip'}
+        for geo_col in ['c_ip_loc', 's_ip_loc']:
+            props[geo_col] = {'type': 'geo_point'}
+        template_name = self.INDEX_PREFIX + '_template'
+        index_pattern = self.INDEX_PREFIX + '-*'
+        self.esu.create_template(template_name, index_pattern, props)
+
+    def run(self):
+        body = {
+            "_source": ["file", "offset"],
+            "query": {"match_all": {}},
+            "sort": [
+                {"file": {"order": "desc"}},
+                {"offset": {"order": "desc"}}
+            ]
+        }
+
+        recs = lnr.esu.recs_search(index=lnr.INDEX_PREFIX + '-*', doc_type=lnr.INDEX_PREFIX, body=body, size=1,
+                                   include_id=False)
+        if len(recs) == 0:
+            last_file, last_offset = None, -1
+        else:
+            last_file, last_offset = recs[0]['file'], recs[0]['offset']
+        logging.info("Processing from Last File: {} Last Offset: {}".format(last_file, last_offset))
+        summary = elasticsearch.helpers.bulk(self.esu._es, self.getrec(last_file, last_offset))
+        logging.info("Loaded {} records".format(self.total_count))
+        self.esu._es.indices.refresh(self.INDEX_PREFIX + '-*')
+        logging.info("Summary: {}".format(summary))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    log_file =  r'N:\TEMP\LOGS\{}.log'.format(os.path.splitext(os.path.basename(sys.argv[0]))[0])
+    print('Logging to: {}'.format(log_file))
+    rv.misc.set_logging(log_file)
+
+    lnr = IISLogReader()
+    if len(sys.argv) > 1:
+        assert sys.argv[1] == 'initialize_template', "Usage: {} [initialialize_template]".format(os.path.basename(sys.argv[0]))
+        lnr.setup_template()
+        sys.exit(0)
+
+    lnr.run()
